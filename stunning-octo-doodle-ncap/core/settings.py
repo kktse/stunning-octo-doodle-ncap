04ea4140c935f93593e8a8ec45050588fac8e867
Kelvin Tse <kelvintse07@gmail.com>
2023-10-02 16:29:06 -0400

    Prep for aws

--- stunning-octo-doodle-ncap/core/settings.py
+++ stunning-octo-doodle-ncap/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l6_7#_t@ic4!)h(s_*p%+*ry(-t-j9=xf$rw(k0$u-67l)ekm7"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-l6_7#_t@ic4!)h(s_*p%+*ry(-t-j9=xf$rw(k0$u-67l)ekm7")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
